d473fc3bd26ee7e7b50b31c5b81e22661791a3bc
Keval Solanki <57882119+keval8solanki@users.noreply.github.com>
2020-03-24 16:13:34 +0530

    Updates

---
+++ precache-manifest.b9d182199c8a57f4ce8183709a3f2ec5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0b5bfb935b2d9d95bfd9",
+    "url": "/reminder/static/css/main.687afabd.chunk.css"
+  },
+  {
+    "revision": "0b5bfb935b2d9d95bfd9",
+    "url": "/reminder/static/js/main.7e1d5320.chunk.js"
+  },
+  {
+    "revision": "4177ae5d1b8f1caf9fc8",
+    "url": "/reminder/static/js/runtime~main.88718075.js"
+  },
+  {
+    "revision": "3c2968ef8b2eb466d66b",
+    "url": "/reminder/static/js/2.32bdae52.chunk.js"
+  },
+  {
+    "revision": "4345c020b5dc11eac0925b2c98b76ee1",
+    "url": "/reminder/index.html"
+  }
+];
