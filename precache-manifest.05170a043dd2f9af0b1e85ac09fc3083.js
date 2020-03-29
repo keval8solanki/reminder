cee05e2cba2a43135af73a7119560bb929102075
Keval Solanki <57882119+keval8solanki@users.noreply.github.com>
2020-03-29 08:27:52 +0530

    Updates

---
+++ precache-manifest.05170a043dd2f9af0b1e85ac09fc3083.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06ca6b889b6b623ab4701b470a602e12",
+    "url": "/reminder/index.html"
+  },
+  {
+    "revision": "1879991b35da1bc204df",
+    "url": "/reminder/static/css/2.0161871a.chunk.css"
+  },
+  {
+    "revision": "d012f0c89bed4e1f2072",
+    "url": "/reminder/static/css/main.697f3add.chunk.css"
+  },
+  {
+    "revision": "1879991b35da1bc204df",
+    "url": "/reminder/static/js/2.a3c72eb8.chunk.js"
+  },
+  {
+    "revision": "0f17c2f9f294fd89a6bf07ffccff9650",
+    "url": "/reminder/static/js/2.a3c72eb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d012f0c89bed4e1f2072",
+    "url": "/reminder/static/js/main.39dc84e0.chunk.js"
+  },
+  {
+    "revision": "0e8934e113ca4337e377",
+    "url": "/reminder/static/js/runtime-main.1a1b7ec5.js"
+  },
+  {
+    "revision": "d463c225c57037a6730e15caadb32c8d",
+    "url": "/reminder/static/media/logout.d463c225.svg"
+  }
+]);
